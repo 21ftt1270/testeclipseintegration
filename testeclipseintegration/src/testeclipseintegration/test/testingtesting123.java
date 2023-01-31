74d036cbf19b13da23f8b1bb38e566c28555c77b
DELL <DELL@DESKTOP-DJUM2VH>
2023-01-31 15:04:14 +0800

    yang first one missing println"GitHub"

--- testeclipseintegration/src/testeclipseintegration/test/testingtesting123.java
+++ testeclipseintegration/src/testeclipseintegration/test/testingtesting123.java
@@ -1,10 +1,8 @@
 package testeclipseintegration.test;
 
 public class testingtesting123 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	System.out.println("GitHub");
+	
 	}
-
 }
