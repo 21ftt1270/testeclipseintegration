07e63dd975fc9aac7201fa2325c40448ef4983c7
DELL <DELL@DESKTOP-DJUM2VH>
2023-01-31 16:18:10 +0800

    changed previous edited ZIK to ZIKRY

--- testeclipseintegration/src/testeclipseintegration/test/testingtesting123.java
+++ testeclipseintegration/src/testeclipseintegration/test/testingtesting123.java
@@ -3,7 +3,7 @@ package testeclipseintegration.test;
 public class testingtesting123 {
 	public static void main(String[] args) {
 	System.out.println("GitHub");
-	ClassB classB = new ClassB("ABCDEFZIK");
+	ClassB classB = new ClassB("ABCDEFZIKRY");
 	System.out.println(classB.getAbc());
 	}
 }
