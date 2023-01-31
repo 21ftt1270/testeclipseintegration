828d82e1a392fc6b45ec2d2c3814c2fe47d77070
DELL <DELL@DESKTOP-DJUM2VH>
2023-01-31 16:04:39 +0800

    update for collaboration

--- testeclipseintegration/src/testeclipseintegration/test/testingtesting123.java
+++ testeclipseintegration/src/testeclipseintegration/test/testingtesting123.java
@@ -3,7 +3,7 @@ package testeclipseintegration.test;
 public class testingtesting123 {
 	public static void main(String[] args) {
 	System.out.println("GitHub");
-	ClassB classA = new ClassB("ABCDEF");
-	System.out.println(classA.getAbc());
+	ClassB classB = new ClassB("ABCDEF");
+	System.out.println(classB.getAbc());
 	}
 }
