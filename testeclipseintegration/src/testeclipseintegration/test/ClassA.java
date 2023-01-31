b19cd2f3a285b2aa2c770047a998cbd446cf9968
DELL <DELL@DESKTOP-DJUM2VH>
2023-01-31 15:28:33 +0800

    addition of class A and updated testingtesting123

---
+++ testeclipseintegration/src/testeclipseintegration/test/ClassA.java
@@ -0,0 +1,18 @@
+package testeclipseintegration.test;
+
+public class ClassA {
+	
+	private String abc;
+	
+	public ClassA(String abc) {
+		this.abc = abc;
+	}
+	
+	public String getAbc() {
+		return abc;
+	}
+	
+	public void setAbc(String abc) {
+		this.abc = abc;
+	}
+}
--- testeclipseintegration/src/testeclipseintegration/test/testingtesting123.java
+++ testeclipseintegration/src/testeclipseintegration/test/testingtesting123.java
@@ -3,6 +3,7 @@ package testeclipseintegration.test;
 public class testingtesting123 {
 	public static void main(String[] args) {
 	System.out.println("GitHub");
-	
+	ClassA classA = new ClassA("ABCDEF");
+	System.out.println(classA.getAbc());
 	}
 }
